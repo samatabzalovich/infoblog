a4cf916ceb029279e7cc5934b8c35c4a85648113
Samat <samatabzalovich@gmail.com>
2022-11-11 04:02:35 +0600

    muha lox

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"flag"
+	"infoblog/internal/models"
+	"log"
+)
+
+type application struct {
+	errorLog  *log.Logger
+	infoLog   *log.Logger
+	infoBlogs *models.InfoBlogsModel
+}
+
 func main() {
+	addr := flag.String("addr", "localhost:4000", "HTTP network address")
+	dbURL := "postgres://postgres:190704Samat@localhost:5432/snippetbox"
+	flag.Parse()
 
 }
