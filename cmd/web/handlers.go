d2c993a2dd28ee798e8b207f74d714d2b33f55af
AmangeldiyevSultan <amangeldievsultan2003@mail.ru>
2022-11-11 13:41:58 +0600

    add

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,1 +1,19 @@
 package main
+
+import "net/http"
+
+func (app *application) home(w http.ResponseWriter, r http.Request) {
+	blogs, err := app.infoBlogs.Insert()
+	if err != nil {
+		app.serverError(v, err)
+		return
+	}
+}
+
+func (app *application) blogView(w http.ResponseWriter, r http.Request) {
+
+}
+
+func (app *application) blogPost(w http.ResponseWriter, r http.Request) {
+
+}
