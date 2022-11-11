42d3123bc95040af3a2133747f7be40ea316612e
Samat <samatabzalovich@gmail.com>
2022-11-11 18:23:33 +0600

    muha lox

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,7 +12,7 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static/", fileServer))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
