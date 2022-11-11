bbead6d266c97b978821662db72bcfa8d0fd9348
Samat <samatabzalovich@gmail.com>
2022-11-11 13:13:14 +0600

    muha lox

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,83 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"github.com/alexedwards/scs/pgxstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"html/template"
 	"infoblog/internal/models"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 type application struct {
-	errorLog  *log.Logger
-	infoLog   *log.Logger
-	infoBlogs *models.InfoBlogsModel
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	infoBlogs      *models.InfoBlogsModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
 	addr := flag.String("addr", "localhost:4000", "HTTP network address")
+	// Define a new command-line flag for the MySQL DSN string.
 	dbURL := "postgres://postgres:190704Samat@localhost:5432/snippetbox"
 	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDB(*&dbURL)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	formDecoder := form.NewDecoder()
+	// Use the scs.New() function to initialize a new session manager. Then we
+	// configure it to use our MySQL database as the session store, and set a
+	// lifetime of 12 hours (so that sessions automatically expire 12 hours
+	// after first being created).
+	sessionManager := scs.New()
+	sessionManager.Store = pgxstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+	// And add the session manager to our application dependencies.
+	app := &application{
+		errorLog:       errorLog,
+		infoLog:        infoLog,
+		infoBlogs:      &models.InfoBlogsModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
+	}
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	// Use the ListenAndServeTLS() method to start the HTTPS server. We
+	// pass in the paths to the TLS certificate and corresponding private key as
+	// the two parameters.
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	errorLog.Fatal(err)
+}
 
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	db, err := pgxpool.Connect(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
